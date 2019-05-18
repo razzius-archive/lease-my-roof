8a43751210813410b48d1468bbcc21ccf48b6e5f
Cameron <cameronjmaccabe@gmail.com>
2019-05-18 12:40:00 -0700

    changed names and updated imports

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
-<<<<<<< HEAD
-import React from "react"
-import logo from "./logo.svg"
-import "./App.css"
-=======
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Logo from "./components";
 import { Home, NoMatch, Quotes, User } from "./pages";
 import './App.css';
->>>>>>> b9028310f30d1f65836c452680ecdd0fc8cbf092
 
 // splash page
-function Apps() {
+function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>hi</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
-=======
     <Router>
       <Logo />
       <Switch>
@@ -40,7 +17,6 @@ function Apps() {
       </Switch>
     </Router>
   );
->>>>>>> b9028310f30d1f65836c452680ecdd0fc8cbf092
 }
 
-export default Ap
+export default App;
