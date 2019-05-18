bd983992c94a944ae4349413df861df2f0e45bff
Cameron <cameronjmaccabe@gmail.com>
2019-05-18 15:29:47 -0700

    added Cart to exports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import Browse from "./Browse"
+import Cart from "./Cart/Cart"
 import Home from "./Home/Home"
+import Login from "./Login/Login"
 import NoMatch from "./NoMatch"
 import Quotes from "./Quotes/Quotes"
 import User from "./User"
 
-export { Browse, Home, NoMatch, Quotes, User }
+export { Browse, Cart, Home, Login, NoMatch, Quotes, User }
