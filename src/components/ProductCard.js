ac93ddfd828f8acace88e8b66959dae78010dc38
Razzi Abuissa <razzi53@gmail.com>
2019-05-18 13:37:26 -0700

    2 rows of product cards

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,8 +1,14 @@
 import React from "react"
 
 export default props => (
-  <div>
-    <img src="http://placehold.it/300" />
-    {props.name}
+  <div style={{ width: "250px", marginBottom: "2em" }}>
+    <img src="http://placehold.it/250" />
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <div>{props.name}</div>
+      <div>{props.price}</div>
+    </div>
+    <div style={{ textAlign: "center", color: "lightgreen" }}>
+      You save {props.savings}
+    </div>
   </div>
 )
--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,17 +1,30 @@
 import React from "react"
 import ProductCard from "../components/ProductCard"
 
-export default () => (
+const ProductRow = props => (
   <div
     style={{
       display: "flex",
-      maxWidth: "700px",
+      maxWidth: "800px",
       margin: "auto",
-      justifyContent: "space-between"
+      justifyContent: "space-around"
     }}
   >
-    <ProductCard name="1" />
-    <ProductCard name="2" />
-    <ProductCard name="3" />
+    {props.children}
+  </div>
+)
+
+export default () => (
+  <div>
+    <ProductRow>
+      <ProductCard name="Product 1" price="$10" savings="$15" />
+      <ProductCard name="Product 2" price="$20" savings="$15" />
+      <ProductCard name="Product 3" price="$30" savings="$15" />
+    </ProductRow>
+    <ProductRow>
+      <ProductCard name="Product 1" price="$10" savings="$15" />
+      <ProductCard name="Product 2" price="$20" savings="$15" />
+      <ProductCard name="Product 3" price="$30" savings="$15" />
+    </ProductRow>
   </div>
 )
